a0449c6b1bb6dc413c7b41c02ff5a15625d836cd
seperool <seperool@gmail.com>
2024-11-06 11:33:27 -0300

    Exemplo numpy

---
+++ Programas/Aula_03/teste_numpy.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  6 10:18:07 2024
+
+@author: Sérgio
+"""
+
+import numpy as np
+
+matriz = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+matriz_transposta = matriz.transpose()
+
+print(matriz)
+
+print(matriz_transposta)
